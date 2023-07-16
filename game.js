1bce4cc2805bf75bddee4890652485d6c427ee33
llthompson <lisalacythompson+github@gmail.com>
2023-07-16 18:33:34 -0500

    game logic working

---
+++ game.js
@@ -0,0 +1,84 @@
+let stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+};
+
+let stone = null
+
+const updateBoard = (stacks) => {
+    Object.keys(stacks).forEach(key => {
+        console.log(key, stacks[key]);
+        const stack = document.getElementById(key)
+        stack.innerHTML = ''
+        stacks[key].forEach(element => {
+            const stone = document.createElement("div");
+            stone.setAttribute("id", element);
+            stone.setAttribute("class", "stone");
+            stone.setAttribute("data-size", element);
+            stack.appendChild(stone)
+        });
+    });
+
+}
+
+const selectRow = (row) => {
+    // const currentRow = row.getAttribute("data-row")
+
+    // console.log("Yay, we clicked an item", row)
+    // console.log("Here is the stone's id: ", row.id)
+    // console.log("Here is the stone's data-size: ", currentRow)
+    console.log('stone ', stone)
+    if (stone === null) {
+        pickUpStone(row.id)
+
+    } else {
+        dropStone(row.id)
+    }
+    updateBoard(stacks)
+    checkForWin()
+}
+
+const pickUpStone = (rowID) => {
+    if (stacks[rowID].length > 0) {
+        stone = stacks[rowID].pop()
+
+    }
+}
+
+const dropStone = (rowID) => {
+    if (stacks[rowID].length === 0 || stacks[rowID][stacks[rowID].length - 1] > stone) {
+        stacks[rowID].push(stone)
+        stone = null
+        console.log(stone)
+    }
+}
+
+const movePiece = (startStack, endStack) => {
+    const blockToMove = stacks[startStack].pop()
+    stacks[endStack].push(blockToMove)
+}
+
+const isLegal = (startStack, endStack) => {
+    if (stacks[endStack].length === 0) {
+        return true
+    }
+    const blockToMove = stacks[startStack][-1]
+    const topBlock = stacks[endStack][-1]
+    return topBlock > blockToMove
+}
+
+const checkForWin = () => {
+    if (JSON.stringify(stacks.b) === JSON.stringify([4, 3, 2, 1]) || JSON.stringify(stacks.c) === JSON.stringify([4, 3, 2, 1])) {
+        alert('You have built the Tower of Hanoi!')
+    }
+}
+
+const towersOfHanoi = (startStack, endStack) => {
+    if (isLegal(startStack, endStack)) {
+        movePiece(startStack, endStack)
+    }
+    checkForWin()
+}
+
+updateBoard(stacks)
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 // * This js file is incomplete. It will log to the console the elements you click
-    // call another function and set stone. You will have to work through the logic
-    // of the game as you know it from building it in the terminal. Work through the
-    // puzzle slowly, stepping through the flow of logic, and making the game work.
-    // Have fun!!
+// call another function and set stone. You will have to work through the logic
+// of the game as you know it from building it in the terminal. Work through the
+// puzzle slowly, stepping through the flow of logic, and making the game work.
+// Have fun!!
 // * First run the program in your browser with live server and double-click on the row you'd like to select an element from.
 // * Why are you get a warning in your console? Fix it.
 // * Delete these comment lines!
 
-const stone = null
+let stone = null
 
 // this function is called when a row is clicked. 
 // Open your inspector tool to see what is being captured and can be used.
 const selectRow = (row) => {
   const currentRow = row.getAttribute("data-row")
-  
+
   console.log("Yay, we clicked an item", row)
   console.log("Here is the stone's id: ", row.id)
   console.log("Here is the stone's data-size: ", currentRow)
+  if (stone === null) {
+    pickUpStone(row.id)
 
-  pickUpStone(row.id)
-} 
+  } else {
+    dropStone(row.id, stone)
+  }
+}
 
 // this function can be called to get the last stone in the stack
 // but there might be something wrong with it...
@@ -34,6 +38,7 @@ const pickUpStone = (rowID) => {
 // Something like: if(!stone){pickupStone} else{dropStone}
 
 const dropStone = (rowID, stone) => {
+  console.log('check row and stone', rowID, stone)
   document.getElementById(rowID).appendChild(stone)
   stone = null
 }
